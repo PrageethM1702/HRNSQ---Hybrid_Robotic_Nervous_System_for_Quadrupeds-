08e8ccdf797b2ec572e334182b8995102b03ed8a
Prageeth Madushankha <prageethm1702@gmail.com>
2026-01-26 16:56:07 +0530

    ESP32 camera streaming functionality

---
+++ Vision/esp32_cam_stream.ino
@@ -0,0 +1,76 @@
+#include "esp_camera.h"
+#include <WiFi.h>
+#include <WebServer.h>
+
+#define CAMERA_MODEL_AI_THINKER
+#include "camera_pins.h"
+
+const char* ssid = "YOUR_WIFI";
+const char* password = "YOUR_PASS";
+
+WebServer server(80);
+
+void handle_jpg_stream() {
+  WiFiClient client = server.client();
+  String response =
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n";
+  server.sendContent(response);
+
+  while (client.connected()) {
+    camera_fb_t * fb = esp_camera_fb_get();
+    if (!fb) continue;
+
+    server.sendContent("--frame\r\n");
+    server.sendContent("Content-Type: image/jpeg\r\n\r\n");
+    server.sendContent((const char*)fb->buf, fb->len);
+    server.sendContent("\r\n");
+
+    esp_camera_fb_return(fb);
+    delay(30); // ~30 FPS
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  config.frame_size = FRAMESIZE_QVGA;
+  config.jpeg_quality = 10;
+  config.fb_count = 2;
+
+  esp_camera_init(&config);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) delay(500);
+
+  Serial.println(WiFi.localIP());
+
+  server.on("/stream", HTTP_GET, handle_jpg_stream);
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+}
